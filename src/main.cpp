0b32e380f3dcbfb7fc274f322b967eccd47ecaba
mhu <marco.huissoon@home.nl>
2026-05-19 15:43:33 +0200

    Setup output

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
-#include <util/delay.h> // Requires that F_CPU is defined in the Makefile
+#define OUTPUT_PIN PB0
+#define BUTTON_PIN PB2
+#define BUTTON_PUE PUEB2
+
+static inline void useOuputPin() {
+    DDRB |= (1 << OUTPUT_PIN); // Set PB0 as output
+    PORTB &= ~(1 << OUTPUT_PIN); // Ensure the output starts LOW
+}
+
+static inline void setup() {
+    cli(); // Disable global interrupts during setup
+
+
+    sei(); // Enable global interrupts
+}
 
 int main(void) {
-    while(1) {
-        _delay_ms(1000);
+
+    setup();
+
+    while (1)
+    {
+
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    
+        sleep_mode(); 
     }
+    
 }
